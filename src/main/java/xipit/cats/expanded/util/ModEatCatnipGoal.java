a4b4e45fbb460fe7c1923452bf6e8578ffadabe2
Hanno Witzleb <hanno2604@gmx.de>
2022-01-07 10:23:54 +0100

    save before trying out eatsweetberriesgoal implementation

--- src/main/java/xipit/cats/expanded/util/ModEatCatnipGoal.java
+++ src/main/java/xipit/cats/expanded/util/ModEatCatnipGoal.java
@@ -1,7 +1,11 @@
 package xipit.cats.expanded.util;
 
 import java.util.EnumSet;
+import java.util.Optional;
 import java.util.function.Predicate;
+
+import org.jetbrains.annotations.Nullable;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -18,6 +22,7 @@ import net.minecraft.world.WorldEvents;
 import net.minecraft.world.WorldView;
 import net.minecraft.world.event.GameEvent;
 import xipit.cats.expanded.block.ModBlocks;
+import xipit.cats.expanded.item.ModItems;
 
 
 // look at BeeEntity$PollinateGoal in findFlower to figure out targetPos
@@ -29,6 +34,8 @@ extends MoveToTargetPosGoal {
     private final World world;
     private int timer;
     private boolean reached;
+    @Nullable
+    BlockPos catnipBushPos;
 
     public ModEatCatnipGoal(PathAwareEntity mob, double speed, int range) {
         super(mob, speed, range, 6);
@@ -40,26 +47,59 @@ extends MoveToTargetPosGoal {
 
     @Override
     public boolean canStart() {
-        if (this.mob.getRandom().nextInt(this.mob.isBaby() ? 50 : 1000) != 0) {
-            return false;
-        }
-        BlockPos blockPos = this.mob.getBlockPos();
-        if (CATNIP_BUSH_PREDICATE.test(this.world.getBlockState(blockPos))) {
+        Optional<BlockPos> optional = this.getCatnipBush();
+        if(optional.isPresent()){
+            //catnipBushPos = optional.get();
+            targetPos = optional.get();
+            startMovingToTarget();
             return true;
         }
-        return this.world.getBlockState(blockPos.down()).isOf(ModBlocks.CATNIP_BUSH);
+        return false;
     }
+
+    private Optional<BlockPos> getCatnipBush() {
+        return this.findCatnipBush(CATNIP_BUSH_PREDICATE, 15.0);
+    }
+
+    private Optional<BlockPos> findCatnipBush(Predicate<BlockState> predicate, double searchDistance) {
+        BlockPos blockPos = mob.getBlockPos();
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+        int i = 0;
+        while ((double)i <= searchDistance) {
+            int j = 0;
+            while ((double)j < searchDistance) {
+                int k = 0;
+                while (k <= j) {
+                    int l;
+                    int n = l = k < j && k > -j ? j : 0;
+                    while (l <= j) {
+                        mutable.set(blockPos, k, i - 1, l);
+                        if (blockPos.isWithinDistance(mutable, searchDistance) && predicate.test(world.getBlockState(mutable))) {
+                            return Optional.of(mutable);
+                        }
+                        l = l > 0 ? -l : 1 - l;
+                    }
+                    k = k > 0 ? -k : 1 - k;
+                }
+                ++j;
+            }
+            i = i > 0 ? -i : 1 - i;
+        }
+        return Optional.empty();
+    }
+
 
     @Override
     public void start() {
         this.timer = this.getTickCount(40);
-        this.world.sendEntityStatus(this.mob, (byte)10);
-        this.mob.getNavigation().stop();
+        // dont know what this does: 
+        //      this.world.sendEntityStatus(this.mob, (byte)10);
     }
 
     @Override
     public void stop() {
         this.timer = 0;
+        this.mob.getNavigation().stop();
     }
 
     @Override
@@ -89,32 +129,23 @@ extends MoveToTargetPosGoal {
         } else {
             this.reached = true;
             --this.tryingTime;
-        }
 
-
-        
-        if (CATNIP_BUSH_PREDICATE.test(this.world.getBlockState(blockPos))) {
-            this.world.setBlockState(blockPos, ModBlocks.CATNIP_BUSH.getDefaultState(), Block.NOTIFY_LISTENERS);
-
-            this.mob.onEatingGrass();
-            this.mob.emitGameEvent(GameEvent.EAT, this.mob.getCameraBlockPos());
-        } else {
-            BlockPos blockPos2 = blockPos.down();
-            if (this.world.getBlockState(blockPos2).isOf(Blocks.GRASS_BLOCK)) {
-                if (this.world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
-                    this.world.syncWorldEvent(WorldEvents.BLOCK_BROKEN, blockPos2, Block.getRawIdFromState(Blocks.GRASS_BLOCK.getDefaultState()));
-                    this.world.setBlockState(blockPos2, Blocks.DIRT.getDefaultState(), Block.NOTIFY_LISTENERS);
-                }
+            if (CATNIP_BUSH_PREDICATE.test(this.world.getBlockState(blockPos))) {
+                this.world.setBlockState(blockPos, ModBlocks.CATNIP_BUSH.getDefaultState(), Block.NOTIFY_LISTENERS);
+    
                 this.mob.onEatingGrass();
                 this.mob.emitGameEvent(GameEvent.EAT, this.mob.getCameraBlockPos());
             }
         }
+
+
     }
 
     @Override
-    protected boolean isTargetPos(WorldView var1, BlockPos var2) {
-        // TODO Auto-generated method stub
-        return false;
+    protected boolean isTargetPos(WorldView world, BlockPos pos) {
+        BlockState blockState = world.getBlockState(pos);
+            return blockState.isOf(ModBlocks.CATNIP_BUSH) && blockState.get(ModBlocks.CATNIP_BUSH.AGE) >= 2;
+        
     }
 }
 
