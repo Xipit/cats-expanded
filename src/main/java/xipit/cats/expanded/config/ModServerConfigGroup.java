fc582b491b36b6168ff004744c40fc501d6ee38a
Hanno Witzleb <hanno2604@gmx.de>
2022-01-24 16:50:08 +0100

    yada yada

--- src/main/java/xipit/cats/expanded/config/ModServerConfigGroup.java
+++ src/main/java/xipit/cats/expanded/config/ModServerConfigGroup.java
@@ -5,6 +5,7 @@ import me.lortseam.completeconfig.api.ConfigGroup;
 
 public class ModServerConfigGroup implements ConfigGroup{
     // Settings that should run on the logical server and not be editable when playing on a dedicated one
+    // not implemented right now, needs a lot of work to sync properly
 
     @Transitive
     public static class SpawningGroup implements ConfigGroup{
