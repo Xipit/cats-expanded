d1b6bf7e4f619ca1b6055157c9047825ee7b1498
Hanno Witzleb <hanno2604@gmx.de>
2022-02-21 19:37:32 +0100

    added random catfacts in console on Initialize

--- src/main/java/xipit/cats/expanded/mixin/ModTitleScreenMixin.java
+++ src/main/java/xipit/cats/expanded/mixin/ModTitleScreenMixin.java
@@ -3,7 +3,10 @@ package xipit.cats.expanded.mixin;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.TitleScreen;
+import xipit.cats.expanded.CatsExpandedClient;
 import xipit.cats.expanded.CatsExpandedMod;
+
+import java.util.Random;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,9 +15,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(TitleScreen.class)
-public class ModTitleScreenMixin {
+public abstract class ModTitleScreenMixin {
+	Random rand = new Random();
+
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		CatsExpandedMod.LOGGER.info("MEOW");
+		
+		int index = rand.nextInt(CatsExpandedClient.CATFACTS.length);
+
+		CatsExpandedMod.LOGGER.info("Random Catfact: " + CatsExpandedClient.CATFACTS[index]);
 	}
 }
+
