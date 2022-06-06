e4533af08fd556c493bef513062e3321b62a85b4
Hanno Witzleb <hanno2604@gmx.de>
2022-06-06 18:11:46 +0200

    [v1.1.0] version increment + small code adjustments

--- src/main/java/xipit/cats/expanded/mixin/GrowCropsGoalMixin.java
+++ src/main/java/xipit/cats/expanded/mixin/GrowCropsGoalMixin.java
@@ -1,5 +1,6 @@
 package xipit.cats.expanded.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,9 +22,9 @@ import org.spongepowered.asm.mixin.injection.At;
 public abstract class GrowCropsGoalMixin
 {
     // The synthetic field of BeeEntity.this
+    @Final
     @Shadow
-    private BeeEntity field_20373;
-
+    BeeEntity field_20373;
 
     @Inject(method = "tick", at = @At("TAIL"))
     protected void InjectTick(CallbackInfo ci){
