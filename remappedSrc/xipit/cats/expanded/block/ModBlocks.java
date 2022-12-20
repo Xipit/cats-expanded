fbc4f7b3d746fad3eee9ce4f55f8d854fa4f802e
Hanno Witzleb <hanno2604@gmx.de>
2022-12-20 15:40:04 +0100

    [WIP]

--- remappedSrc/xipit/cats/expanded/block/ModBlocks.java
+++ remappedSrc/xipit/cats/expanded/block/ModBlocks.java
@@ -1,7 +1,6 @@
 package xipit.cats.expanded.block;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
 import net.minecraft.sound.BlockSoundGroup;
 import xipit.cats.expanded.CatsExpandedMod;
 import xipit.cats.expanded.util.RegistryHelper;
@@ -11,11 +10,18 @@ public class ModBlocks {
     public static final CatnipBushBlock CATNIP_BUSH
             = new CatnipBushBlock(AbstractBlock.Settings.of(Material.PLANT).ticksRandomly().noCollision().sounds(BlockSoundGroup.SWEET_BERRY_BUSH).nonOpaque());
 
+    public static final Block CARVED_CAT_PUMPKIN
+            = new CarvedPumpkinBlock(AbstractBlock.Settings.of(Material.GOURD, MapColor.ORANGE).strength(1.0f).sounds(BlockSoundGroup.WOOD).allowsSpawning(RegistryHelper::always));
+    public static final Block CAT_O_LANTERN
+            = new CarvedPumpkinBlock(AbstractBlock.Settings.of(Material.GOURD, MapColor.ORANGE).strength(1.0f).sounds(BlockSoundGroup.WOOD).luminance(state -> 15).allowsSpawning(RegistryHelper::always));
 
     // function called on mod initalization
     public static void register() {
         CatsExpandedMod.LOGGER.info("Registering ModBlocks");
 
         RegistryHelper.registerBlock("catnip_bush", CATNIP_BUSH);
+
+        RegistryHelper.registerBlock("carved_cat_pumpkin", CARVED_CAT_PUMPKIN);
+        RegistryHelper.registerBlock("cat_o_lantern", CAT_O_LANTERN);
     }
 }
--- remappedSrc/xipit/cats/expanded/item/ModItems.java
+++ remappedSrc/xipit/cats/expanded/item/ModItems.java
@@ -14,25 +14,54 @@ public class ModItems {
 
     // Translationkey for CatearArmor is hardcoded --> wont change with different ids
     public static final CatearArmor CATEARS = new CatearArmor(EquipmentSlot.HEAD, CATEAR_GROUP);
-    public static final CatearArmor CATEARS_CYAN = new CatearArmor("cyan", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_WHITE = new CatearArmor("white", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_BLACK = new CatearArmor("black", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_GRAY = new CatearArmor("gray", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_LIGHT_GRAY = new CatearArmor("light_gray", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_RED = new CatearArmor("red", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_ORANGE = new CatearArmor("orange", EquipmentSlot.HEAD, CATEAR_GROUP);
     public static final CatearArmor CATEARS_MAGENTA = new CatearArmor("magenta", EquipmentSlot.HEAD, CATEAR_GROUP);
     public static final CatearArmor CATEARS_PURPLE = new CatearArmor("purple", EquipmentSlot.HEAD, CATEAR_GROUP);
-    public static final CatearArmor CATEARS_RED = new CatearArmor("red", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_PINK = new CatearArmor("pink", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_YELLOW = new CatearArmor("yellow", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_BLUE = new CatearArmor("blue", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_LIGHT_BLUE = new CatearArmor("light_blue", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_CYAN = new CatearArmor("cyan", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_GREEN = new CatearArmor("green", EquipmentSlot.HEAD, CATEAR_GROUP);
+    public static final CatearArmor CATEARS_LIME = new CatearArmor("lime", EquipmentSlot.HEAD, CATEAR_GROUP);
     public static final CatearArmor CATEARS_RAINBOW = new CatearArmor("rainbow", EquipmentSlot.HEAD, CATEAR_GROUP);
+
 
     public static final AliasedBlockItem CATNIP = new CatnipItem(ModBlocks.CATNIP_BUSH, new Item.Settings().group(CatsExpandedMod.CATEAR_GROUP));
 
+    public static final AliasedBlockItem CARVED_CAT_PUMPKIN = new AliasedBlockItem(ModBlocks.CARVED_CAT_PUMPKIN, new Item.Settings().group(CatsExpandedMod.CATEAR_GROUP));
+    public static final AliasedBlockItem CAT_O_LANTERN = new AliasedBlockItem(ModBlocks.CAT_O_LANTERN, new Item.Settings().group(CatsExpandedMod.CATEAR_GROUP));
 
     public static void register() {
         CatsExpandedMod.LOGGER.info("Registering ModItems");
 
         RegistryHelper.registerItem("catears", CATEARS);
-        RegistryHelper.registerItem("catears_cyan", CATEARS_CYAN);
+        RegistryHelper.registerItem("catears_white", CATEARS_WHITE);
+        RegistryHelper.registerItem("catears_black", CATEARS_BLACK);
+        RegistryHelper.registerItem("catears_gray", CATEARS_GRAY);
+        RegistryHelper.registerItem("catears_light_gray", CATEARS_LIGHT_GRAY);
+        RegistryHelper.registerItem("catears_red", CATEARS_RED);
+        RegistryHelper.registerItem("catears_orange", CATEARS_ORANGE);
         RegistryHelper.registerItem("catears_magenta", CATEARS_MAGENTA);
         RegistryHelper.registerItem("catears_purple", CATEARS_PURPLE);
-        RegistryHelper.registerItem("catears_red", CATEARS_RED);
+        RegistryHelper.registerItem("catears_pink", CATEARS_PINK);
+        RegistryHelper.registerItem("catears_yellow", CATEARS_YELLOW);
+        RegistryHelper.registerItem("catears_blue", CATEARS_BLUE);
+        RegistryHelper.registerItem("catears_light_blue", CATEARS_LIGHT_BLUE);
+        RegistryHelper.registerItem("catears_cyan", CATEARS_CYAN);
+        RegistryHelper.registerItem("catears_green", CATEARS_GREEN);
+        RegistryHelper.registerItem("catears_lime", CATEARS_LIME);
         RegistryHelper.registerItem("catears_rainbow", CATEARS_RAINBOW);
 
         RegistryHelper.registerItem("catnip", CATNIP);
+
+        RegistryHelper.registerItem("carved_cat_pumpkin", CARVED_CAT_PUMPKIN);
+        RegistryHelper.registerItem("cat_o_lantern", CAT_O_LANTERN);
+
     }
 }
